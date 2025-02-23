c614667eeadc5ae264597e6a39354eb62558f27b
Hemantsh-79833 <hs908463@gmail.com>
2025-02-23 17:41:02 +0530

    Add files via upload

---
+++ compoundinterest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+// C program to calculate Compound Interest
+int main() 
+{
+  double principal = 10000; 
+  double rate = 5; 
+
+  double time = 2; 
+  double Amount = principal * 
+                  ((pow((1 + rate / 100), 
+                    time)));
+  double CI = Amount - principal;
+ 
+  printf("Compound Interest is : %lf",CI);
+  return 0;
+}
